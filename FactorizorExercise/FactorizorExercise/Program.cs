a8986c67c0c7576769c6667a7ec8cd39e9c98f52
Nate Klein <natek21@gmail.com>
2017-08-22 13:57:25 -0400

    Adding the Restaurant Pages

---
+++ FactorizorExercise/FactorizorExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorizorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to factor?");
+            int number = GetNumberFromUser();
+
+            Calculator.PrintFactors(number);
+            Calculator.IsPerfectNumber(number);
+            Calculator.IsPrimeNumber(number);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+        static int GetNumberFromUser()
+        {
+            
+        }
+    }
+
+    class Calculator
+    {
+        /// <summary>
+        /// Given a number, print the factors per the specification
+        /// </summary>
+        public static void PrintFactors(int number)
+        {
+
+        }
+
+        /// <summary>
+        /// Given a number, print if it is perfect or not
+        /// </summary>
+        public static void IsPerfectNumber(int number)
+        {
+
+        }
+
+        /// <summary>
+        /// Given a number, print if it is prime or not
+        /// </summary>
+        public static void IsPrimeNumber(int number)
+        {
+
+        }
+    }
+}
--- HelloGuild/HelloGuild/Program.cs
+++ HelloGuild/HelloGuild/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloGuild
+namespace NullThings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Guild!");
+
+            Console.WriteLine("The cow says, \"Moo\"");
+            Console.ReadLine();
         }
     }
 }
