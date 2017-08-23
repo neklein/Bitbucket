b4884491f8768f98b161b876234f2af1486bd227
Nate Klein <natek21@gmail.com>
2017-08-23 16:00:10 -0400

    GuessingGame actually completed this time

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -10,6 +10,7 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine(name + ", you can quit at any time by pressing Q.");
@@ -18,9 +19,10 @@ namespace GuessingGame
             int guess = 0;
             Random rng = new Random();
             answer = rng.Next(1, 20);
-
+            int NumGuesses = 0;
             while (true)
             {
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine(name + ", Enter a guess between 1 and 20");
                 input = Console.ReadLine();
                 if (input == "Q")
@@ -31,21 +33,36 @@ namespace GuessingGame
                 {
                     if (guess == answer)
                     {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine(name + ", you guessed right! The answer is {0}", answer);
+                        NumGuesses++;
+                        if(NumGuesses > 1)
+                        {
+                            Console.WriteLine(name + ", you guessed the right answer in " + NumGuesses + " attempts");
+                        }
+                        if (NumGuesses == 1)
+                        {
+                            Console.WriteLine("Wow! You got it right on the first try. Way to go, " + name + "!");
+                        }
                         break;
                     }
                     if (guess < 1 || guess > 20)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine(name + ", your guess should be between 1 and 20.");
                         continue;
                     }
                     if (guess > answer)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine(name + ", Lower!");
+                        NumGuesses++;
                     }
                     if (guess < answer)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine(name + ", Higher!");
+                        NumGuesses++;
                     }
                 }
                
---
+++ String-Array Practice/String-Array Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Array_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "I lik chs";
+            int NumOfEs = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                i = s.IndexOf("e", i);
+                if(i == -1)
+                {
+                    Console.WriteLine("There is no e");
+                }
+                Console.WriteLine($"I found an e at index: {i}");
+                
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
